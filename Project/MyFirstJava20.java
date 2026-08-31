82f06d8220b1959eae9d4923034b2eaf320a6714
Muhammad Andhany Bahar <141795737+andhanybahar@users.noreply.github.com>
2026-08-31 12:29:14 +0700

    Add files via upload

---
+++ Project/MyFirstJava20.java
@@ -0,0 +1,7 @@
+public class MyFirstJava20 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("\nThis is my first Java program.");
+        System.out.print("\nI am learning Java programming.");
+    }
+}
---
+++ Project/RobotArm20.java
@@ -0,0 +1,14 @@
+public class RobotArm20 {
+    public static void main(String[] args) {
+        System.out.println("Ambil bola di nampan A, taruh di nampan C");
+        // System.out.print("Ambil bola di nampan B, taruh di nampan A");
+        // System.out.print("Ambil bola di nampan C, taruh di nampan B"); 
+
+        // System.out.print("Setelah Langkah Pertama: Nampan A Berpindah Posisi ke Nampan C");
+        // System.out.print("Setelah Langkah Kedua: Nampan B Berpindah Posisi ke Nampan A");
+        // System.out.print("Setelah Langkah Ketiga: Nampan C Berpindah Posisi ke Nampan B");
+
+        // System.out.print("Kesimpulan: (e) nampan C kosong");
+        // System.out.print("Kesimpulan: (f) tidak ada yang berubah, tiap bola kembali ke tempat asalnya");
+    }
+}
